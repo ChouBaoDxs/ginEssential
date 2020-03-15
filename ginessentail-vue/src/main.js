e35360d873ea58711cede02e03f4aba3a862b6db
ChouBaoDxs <826842697@qq.com>
2020-03-16 00:28:11 +0800

    011 引入bootstrap-vue、登录和注册路由、导航条

--- ginessentail-vue/src/main.js
+++ ginessentail-vue/src/main.js
@@ -1,9 +1,17 @@
 import Vue from 'vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// scss style
+import './assets/scss/index.scss'
+
 Vue.config.productionTip = false
+
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
 
 new Vue({
   router,
--- ginessentail-vue/src/router/index.js
+++ ginessentail-vue/src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Register from '../views/register/Register.vue'
+// import Login from '../views/login/Login.vue'
 
 Vue.use(VueRouter)
 
@@ -17,6 +19,16 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+  },
+  {
+    path: '/register',
+    name: 'register',
+    component: Register,
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: () => import('../views/login/Login.vue'), // 这样写成函数的好处是懒加载
   },
 ]
 
