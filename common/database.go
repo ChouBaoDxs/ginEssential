64833e2938e13e1e2fb5e5392099ee97036f1193
ChouBaoDxs <826842697@qq.com>
2020-03-15 23:12:44 +0800

    008 从文件中读取配置——viper

--- common/database.go
+++ common/database.go
@@ -4,18 +4,19 @@ import (
 	"fmt"
 	"ginEssential/model"
 	"github.com/jinzhu/gorm"
+	"github.com/spf13/viper"
 )
 
 var DB *gorm.DB
 
 func InitDB() *gorm.DB {
-	driverName := "mysql"
-	host := "localhost"
-	port := "3306"
-	database := "ginessential"
-	username := "root"
-	password := "123456"
-	charset := "utf8"
+	driverName := viper.GetString("datasource.driverName")
+	host := viper.GetString("datasource.host")
+	port := viper.GetString("datasource.port")
+	database := viper.GetString("datasource.database")
+	username := viper.GetString("datasource.username")
+	password := viper.GetString("datasource.password")
+	charset := viper.GetString("datasource.charset")
 	args := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=%s&parseTime=true",
 		username,
 		password,
--- main.go
+++ main.go
@@ -4,13 +4,33 @@ import (
 	"ginEssential/common"
 	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/spf13/viper"
+	"log"
+	"os"
 )
 
 func main() {
+	InitConfig()
 	db := common.InitDB()
 	defer db.Close()
 
 	r := gin.Default()
 	r = CollectRoute(r)
+	port := viper.GetString("server.port")
+	if port != "" {
+		panic(r.Run(":" + port))
+	}
 	panic(r.Run())
+}
+
+func InitConfig() {
+	workDir, _ := os.Getwd()
+	viper.SetConfigName("application")
+	viper.SetConfigType("yml")
+	viper.AddConfigPath(workDir + "/config")
+	err := viper.ReadInConfig()
+	if err != nil {
+		log.Printf("read config failed ,err : %v", err)
+		panic("read config failed, err: " + err.Error())
+	}
 }
