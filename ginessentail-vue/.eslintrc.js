dc89d59faaa0fe6f106e98d0acd07481f434c6e0
ChouBaoDxs <826842697@qq.com>
2020-03-15 23:46:46 +0800

    010 创建项目代码规范

---
+++ ginessentail-vue/.eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    'plugin:vue/essential',
+    '@vue/airbnb',
+  ],
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
+  rules: {
+    'indent': ['error', 2],
+    'semi': ['error', 'never'],
+    'quotes': ['error', 'single'],
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+  },
+};
